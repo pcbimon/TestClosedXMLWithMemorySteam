e059fcb293b2120b33381ee15ff5e59bc8691daa
Patipat Chewprehca <patipat.che@mahidol.edu>
2021-03-17 08:53:40 +0700

    Add project files.

---
+++ TestClosedXML/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using ClosedXML.Excel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using TestClosedXML.Models;
+
+namespace TestClosedXML.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult excel()
+        {
+            IXLWorkbook wb = new XLWorkbook();
+            IXLWorksheet ws = wb.Worksheets.Add("Sample Sheet");
+
+            ws.Cell(1,1).Value = "Hello World!";
+            byte[] xlsInBytes;
+
+            using (MemoryStream memoryStream = SaveWorkbookToMemoryStream((XLWorkbook) wb))
+            {
+                xlsInBytes = memoryStream.ToArray();
+            }
+            wb.Dispose();
+            return File(xlsInBytes, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+                , "Report.xlsx");
+            
+        }
+        public static MemoryStream SaveWorkbookToMemoryStream(XLWorkbook workbook)
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                workbook.SaveAs(stream, new SaveOptions { EvaluateFormulasBeforeSaving = false, GenerateCalculationChain = false,
+                    ValidatePackage = false });
+                return stream;
+            }
+        }
+    }
+}
